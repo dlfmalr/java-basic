d84479e133b8a9c05fe08d8ca52d26a35ddc8823
dlfmalr <sonhyeseung00@gmail.com>
2024-03-14 17:48:35 +0900

    java test

---
+++ src/Exam/Array.java
@@ -0,0 +1,21 @@
+package Exam;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] num = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+        for(int i = 0; i < 3; i++) {
+            System.out.println(num[0]);
+            System.out.println(num[1]);
+            System.out.println(num[2]);
+            System.out.println(num[3]);
+            System.out.println(num[4]);
+            System.out.println(num[5]);
+            System.out.println(num[6]);
+            System.out.println(num[7]);
+            System.out.println(num[8]);
+            System.out.println(num[9]);
+        }
+
+    }
+}
--- src/Exam/DoubleLoop.java
+++ src/Exam/DoubleLoop.java
@@ -17,5 +17,13 @@ public class DoubleLoop {
         int n = 4; // 시작
         int m = 20; // 끝
         int limit = 10; // 곱의 개수
+
+        for(int i = 4; i < 20; i++) {
+            if(i % 2 == 1) {
+                for(int j = 2; j <= 10; j+=2) {
+                    System.out.println(i + " * " + j + " = " + i*j);
+                }
+            }
+        }
     }
 }
